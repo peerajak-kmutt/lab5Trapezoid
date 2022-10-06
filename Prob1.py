707e087d85dbe722fc2d3066e735766cbc8d516e
peerajak-kmutt <111479721+peerajak-kmutt@users.noreply.github.com>
2022-10-06 20:03:46 +0700

    Update Prob1.py

--- Prob1.py
+++ Prob1.py
@@ -1,4 +1,4 @@
-def trapezoid(f,a,b,n):
+def trapezoid(f,a=0,b=1,n=60):
     # f is the function  f(x)
     # a is the x=a, start point of the integration
     # b is the x=b, end point of the integration
